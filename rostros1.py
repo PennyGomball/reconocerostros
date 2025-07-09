97b4d5e8d2a8e53e9db6714df2635fdd35f4d4a5
Salvador <salvador.diaz@usach.cl>
2025-07-09 14:27:08 +0000

    camara 0

--- rostros1.py
+++ rostros1.py
@@ -4,7 +4,7 @@ face_classifier = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
 )
 
-video_capture = cv2.VideoCapture(0)
+video_capture = cv2.VideoCapture
 
 def detect_bounding_box(vid):
     gray_image = cv2.cvtColor(vid, cv2.COLOR_BGR2GRAY)
